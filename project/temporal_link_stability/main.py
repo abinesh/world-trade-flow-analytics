4c2aa5a53456d77127e7f7e61904f1262a2009b8
abinesh <td.abinesh@gmail.com>
2012-11-12 15:24:07 -0800

    writing generated matlab code to a file, updated single graph matlab code

--- project/temporal_link_stability/main.py
+++ project/temporal_link_stability/main.py
@@ -9,13 +9,14 @@ def file_safe(country):
     return country.replace(',', '_').replace('.', '_').replace(' ', '_')
 
 
-def read_file(file_path):
-    reader = csv.DictReader(open(file_path, 'rb'), skipinitialspace=True)
-
+def read_file(input_file, output_file):
+    i=0
+    reader = csv.DictReader(open(input_file, 'rb'), skipinitialspace=True)
+    out = open(output_file, 'w')
     for row in reader:
         importer = row.get('Importer')
         exporter = row.get('Exporter')
-        if (importer == 'World' or exporter == 'World') or (importer == exporter) or exporter != 'USA':
+        if (importer == 'World' or exporter == 'World') or (importer == exporter):
             continue
         if not is_valid_country(importer) or not is_valid_country(exporter):
             continue
@@ -34,16 +35,18 @@ def read_file(file_path):
                 year = column_to_year(column)
                 writer.writerow([year, export_quantity])
 
-        print "clear"
-        print "data = load('" + filepath + "')"
-        print "x = data(:,1)"
-        print "y = data(:,2)"
-        print "ylinearfit = polyval(polyfit(x,y,1),x)"
-        print "yquadfit = polyval(polyfit(x,y,2),x)"
-        print "plot(x,y,'k-s',x,ylinearfit,x,yquadfit)"
-        print "xlabel('Year')"
-        print "ylabel('Export Quantity')"
-        print "saveas(gcf,'out/wtf/" + file_safe(exporter) + "-export-to-" + file_safe(importer) + "','jpg')"
+        out.write("clear\n")
+        out.write("data = load('" + filepath + "')\n")
+        out.write("x = data(:,1)\n")
+        out.write("y = data(:,2)\n")
+        out.write("ylinearfit = polyval(polyfit(x,y,1),x)\n")
+        out.write("yquadfit = polyval(polyfit(x,y,2),x)\n")
+        out.write("plot(x,y,'k-s',x,ylinearfit,x,yquadfit)\n")
+        out.write("xlabel('Year')\n")
+        out.write("ylabel('Export Quantity')\n")
+        out.write("saveas(gcf,'out/wtf/" + file_safe(exporter) + "-export-to-" + file_safe(importer) + "','jpg')\n")
+        print i
+        i+=1
 
 
-read_file(WORLD_TRADE_FLOW_DATA_FILE)
+read_file(WORLD_TRADE_FLOW_DATA_FILE, 'matlab/generateplots.m')
