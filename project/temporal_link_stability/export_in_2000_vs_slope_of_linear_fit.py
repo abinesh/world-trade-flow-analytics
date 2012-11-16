f75b929c6a19dd30e23b55eba82b715697241f5c
abinesh <td.abinesh@gmail.com>
2012-11-16 09:49:12 -0800

    writing slope-vs-export to files

--- project/temporal_link_stability/export_in_2000_vs_slope_of_linear_fit.py
+++ project/temporal_link_stability/export_in_2000_vs_slope_of_linear_fit.py
@@ -1,6 +1,6 @@
 import re
 from project import countries
-from project.config import WORLD_TRADE_FLOW_DATA_FILE
+from project.config import WORLD_TRADE_FLOW_DATA_FILE, WRITE_OUT_TO_DIR
 from project.traids_vs_degree_plot.export_data.exportdata import  load_export_data, export_data
 from project.util import file_safe
 
@@ -33,11 +33,24 @@ def trade_relationship_exists(exporter, importer):
             return True
     return False
 
-for exporter in countries.countries:
-    for importer in countries.countries:
-        if exporter == importer:
-            continue
-        if not trade_relationship_exists(exporter, importer):
-            continue
-        print exporter + ' ' + importer
-        print str(slope_data(exporter, importer)) + ' ' + str(export_data(2000, exporter, importer))
+
+def write_data_files_for_slope_vs_export_plots(out_dir):
+    for exporter in countries.countries:
+        f = open(out_dir + '/' + file_safe(exporter) + '.txt', 'w')
+        f_world = open(out_dir + '/' + file_safe(exporter) + '-world.txt', 'w')
+        for importer in countries.countries:
+            if exporter == importer:
+                continue
+            if not trade_relationship_exists(exporter, importer):
+                continue
+            print exporter + ' ' + importer
+            print str(slope_data(exporter, importer)) + ' ' + str(export_data(2000, exporter, importer))
+            f.write(str(slope_data(exporter, importer)) + ' ' + str(export_data(2000, exporter, importer)) + '\n')
+            if importer == 'World':
+                f_world.write(
+                    str(slope_data(exporter, importer)) + ' ' + str(export_data(2000, exporter, importer)) + '\n')
+
+        f.close()
+        f_world.close()
+
+write_data_files_for_slope_vs_export_plots('matlab/out/slope-vs-export')
