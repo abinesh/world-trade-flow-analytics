417db75f8746dd862f405a089db42b799fa9fe62
abinesh <td.abinesh@gmail.com>
2012-12-03 16:06:18 -0800

    Created a function to generate network graph data for structural balance

---
+++ project/structural_balance/config.py
@@ -0,0 +1,4 @@
+JSON_OUT_DIR = 'out/'
+
+def output_file(year):
+    return JSON_OUT_DIR + "%s.json" % year
--- project/structural_balance/main.py
+++ project/structural_balance/main.py
@@ -1,6 +1,6 @@
-from project import countries
 from project.config import  WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL
 from project.countries import country_pairs
+from project.structural_balance.config import output_file
 from project.traids_vs_degree_plot.config import STRONG_TIES_LOWER_BOUND, STRONG_TIES_UPPER_BOUND
 from project.traids_vs_degree_plot.export_data.exportdata import load_export_data, export_data
 from project.traids_vs_degree_plot.export_data.strongties import is_there_a_strong_tie_method_B
@@ -10,12 +10,17 @@ load_export_data(WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL, should_include_world=True)
 def total_exports(exporter, year):
     return export_data(year, exporter, 'World')
 
-count = 0
-for (c1, c2) in country_pairs():
-    if count == 2000:
-        break
-    type = "suit" if is_there_a_strong_tie_method_B(2000, c1, c2, STRONG_TIES_LOWER_BOUND,
-        STRONG_TIES_UPPER_BOUND) else "resolved"
-    print   '{source:"%s", target:"%s", type:"%s"},' % (c1, c2, type)
-    count += 1
-    pass
+
+def generate_network_graph_data_for_structured_balance(year, max_country_pairs,out_file):
+    f = open(out_file, 'w')
+    count = 0
+    for (c1, c2) in country_pairs():
+        if count == max_country_pairs:
+            break
+        type = "suit" if is_there_a_strong_tie_method_B(year, c1, c2, STRONG_TIES_LOWER_BOUND,
+            STRONG_TIES_UPPER_BOUND) else "resolved"
+        f.write('{source:"%s", target:"%s", type:"%s"},\n' % (c1, c2, type))
+        count += 1
+    f.close()
+
+generate_network_graph_data_for_structured_balance(2000, -1, output_file(2000))
