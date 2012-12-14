40357d0d50fcd11b53cdd9657b6e15dd2975a90c
abinesh <td.abinesh@gmail.com>
2012-12-13 20:03:00 -0800

    A bit of refactoring, polymorphism in link definitions

--- project/structural_balance/definitions.py
+++ project/structural_balance/definitions.py
@@ -1,6 +1,26 @@
 from project.export_data.strongties import is_there_a_strong_tie_method_B
-from project.traids_vs_degree_plot.config import STRONG_TIES_LOWER_BOUND, STRONG_TIES_UPPER_BOUND
 
-def definition_A(c1, c2, data, year):
-    return "positive" if is_there_a_strong_tie_method_B(data, year, c1, c2, STRONG_TIES_LOWER_BOUND,
-        STRONG_TIES_UPPER_BOUND) else "negative"
+NEGATIVE_LINK = "negative"
+POSITIVE_LINK = "positive"
+NO_LINK = "null"
+
+def args_for_definition_A(lower_bound, upper_bound):
+    return {
+        'lower_bound': lower_bound,
+        'upper_bound': upper_bound
+    }
+
+
+def args_for_definition_B():
+    return {}
+
+
+def definition_A(data, year, country_A, country_B, args):
+    return POSITIVE_LINK if is_there_a_strong_tie_method_B(data, year, country_A, country_B, args.lower_bound,
+        args.upper_bound) else NEGATIVE_LINK
+
+
+def definition_B(data, year, country_A, country_B, args):
+    return POSITIVE_LINK
+
+
--- project/structural_balance/main.py
+++ project/structural_balance/main.py
@@ -2,16 +2,23 @@ from project.config import  WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL
 from project.countries import country_pairs, a_subset_of_countries
 from project.export_data.exportdata import ExportData
 from project.structural_balance.config import output_file
-from project.structural_balance.definitions import definition_A
+from project.structural_balance.definitions import definition_A, NO_LINK, args_for_definition_A, args_for_definition_B, definition_B
+from project.traids_vs_degree_plot.config import STRONG_TIES_LOWER_BOUND, STRONG_TIES_UPPER_BOUND
 
 
-def generate_network_graph_data_for_structured_balance_definition_A(data, year, subset_of_countries, out_file):
+def generate_network_graph_data(data, year, subset_of_countries, out_file, definition, args):
     f = open(out_file, 'w')
     for (c1, c2) in country_pairs(subset_of_countries):
-        f.write('{source:"%s", target:"%s", type:"%s"},\n' % (c1, c2, definition_A(c1, c2, data, year)))
+        link_type = definition(data, year, c1, c2, args)
+        if link_type != NO_LINK:
+            f.write('{source:"%s", target:"%s", type:"%s"},\n' % (c1, c2, link_type))
     f.close()
 
 data = ExportData()
 data.load_export_data(WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL, should_include_world=True)
 
-generate_network_graph_data_for_structured_balance_definition_A(data, 2000, a_subset_of_countries, output_file(2000))
+generate_network_graph_data(data, 2000, a_subset_of_countries, output_file(2000), definition_A,
+    args_for_definition_A(STRONG_TIES_LOWER_BOUND, STRONG_TIES_UPPER_BOUND))
+
+generate_network_graph_data(data, 2000, None, output_file(2000), definition_B, args_for_definition_B())
+
