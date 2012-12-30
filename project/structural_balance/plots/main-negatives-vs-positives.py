1098845464657448c3e46b6e5df84afce8d8089b
abinesh <td.abinesh@gmail.com>
2012-12-29 21:54:37 -0700

    Temporal scatter plot in d3: Number of positive links vs number of negative links: Generated json data

--- project/structural_balance/plots/main-negatives-vs-positives.py
+++ project/structural_balance/plots/main-negatives-vs-positives.py
@@ -1,13 +1,12 @@
+import json
 from project import countries
 from project.config import WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL
 from project.export_data.exportdata import ExportData
 from project.structural_balance.definitions import definition_B, args_for_definition_B, NEGATIVE_LINK, POSITIVE_LINK
 from project.util import file_safe
 
-data = ExportData()
-data.load_export_data('../' + WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL, should_include_world=True)
 
-def print_positives_and_negatives(data, definition, def_args, out_dir):
+def positives_and_negatives_data_matlab(data, definition, def_args, out_dir):
     in_files = open(out_dir + 'in_files.txt', 'w')
     for year in data.all_years:
         file_name = 'y%s' % year
@@ -28,7 +27,48 @@ def print_positives_and_negatives(data, definition, def_args, out_dir):
         data_file.close()
     in_files.close()
 
-print_positives_and_negatives(data, definition_B, args_for_definition_B(5), 'out/')
+
+def positives_and_negatives_data_d3(data, definition, def_args, out_dir):
+#    countries_list = ["USA","UK","Turkey"]
+    countries_list = countries.countries
+
+    class NationData:
+        def __init__(self, country_name):
+            self.name = country_name
+            self.region = country_name
+            self.positive_links_count = []
+            self.negative_links_count = []
+            self.export_quantity = []
+
+        def add_data(self, year, p, n, ex):
+            self.positive_links_count.append([year, p])
+            self.negative_links_count.append([year, n])
+            self.export_quantity.append([year, ex])
+
+    all_nations_dict = []
+
+    json_file = open(out_dir + 'nations_gen.json', 'w')
+    for A in countries_list:
+        nation = NationData(A)
+        for year in data.all_years:
+            positive = 0
+            negative = 0
+            for B in countries_list:
+                if A == B: continue
+                link_type = definition(data, year, A, B, def_args)
+                if link_type == NEGATIVE_LINK: negative += 1
+                elif link_type == POSITIVE_LINK: positive += 1
+            nation.add_data(year, positive, negative, data.total_exports(A, year))
+        all_nations_dict.append(nation.__dict__)
+
+    json_file.write(json.dumps(all_nations_dict))
+    json_file.close()
+
+data = ExportData()
+data.load_export_data('../' + WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL, should_include_world=True)
+
+positives_and_negatives_data_d3(data, definition_B, args_for_definition_B(5), 'out/')
+#positives_and_negatives_data_matlab(data, definition_B, args_for_definition_B(5), 'out/')
 
 
 '''
