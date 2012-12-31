f31e4086b1b39bc992d9f538dc4cf4332b558ec1
abinesh <td.abinesh@gmail.com>
2012-12-30 23:00:23 -0700

    Removed commented lines

--- project/structural_balance/main.py
+++ project/structural_balance/main.py
@@ -1,9 +1,8 @@
 from project.config import  WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL
-from project.countries import country_pairs, a_subset_of_countries, world_excluded_countries_list
+from project.countries import country_pairs
 from project.export_data.exportdata import ExportData
-from project.structural_balance.config import output_file, output_file_html, html_footer, html_header
-from project.structural_balance.definitions import definition_A, NO_LINK, args_for_definition_A, args_for_definition_B, definition_B
-from project.traids_vs_degree_plot.config import STRONG_TIES_LOWER_BOUND, STRONG_TIES_UPPER_BOUND
+from project.structural_balance.config import  output_file_html, html_footer, html_header
+from project.structural_balance.definitions import  NO_LINK, args_for_definition_B, definition_B
 
 def generate_network_graph_data(data, year, subset_of_countries, out_file, definition, args):
     f = open(out_file, 'w')
@@ -18,9 +17,6 @@ def generate_network_graph_data(data, year, subset_of_countries, out_file, defin
 
 data = ExportData()
 data.load_export_data(WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL, should_read_world_datapoints=True)
-
-#generate_network_graph_data(data, 2000, a_subset_of_countries, output_file(2000), definition_A,
-#    args_for_definition_A(STRONG_TIES_LOWER_BOUND, STRONG_TIES_UPPER_BOUND))
 
 for year in range(1968, 2001):
     top_countries_and_exports = data.top_countries_by_export(year, 50)
