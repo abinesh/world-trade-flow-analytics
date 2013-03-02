55f56d5ead11a728520d98f693bd5cae41a9d8f1
abinesh <td.abinesh@gmail.com>
2013-03-01 17:57:52 -0800

    signed_networks: structural_balance: metrics: edge level: embeddedness: compute_map method written

--- project/signed_networks/structural_balance/metrics/edge.py
+++ project/signed_networks/structural_balance/metrics/edge.py
@@ -1,3 +1,4 @@
+from itertools import combinations
 from project.signed_networks.definitions import POSITIVE_LINK, NO_LINK
 
 max_common_neighbours_possible = 201
@@ -11,3 +12,28 @@ def compute_fraction(map):
             fraction = positive_edges * 1.0 / (positive_edges + negative_edges)
         result.append(fraction)
     return result
+
+
+def common_neighbours_count(data, year, A, B, definition, def_args):
+    common_neighbour_count = 0
+    for C in data.countries():
+        if C == A or C == B: continue
+        if NO_LINK not in [definition(data, year, A, C, def_args), definition(data, year, B, C, def_args)]:
+            common_neighbour_count += 1
+    return common_neighbour_count
+
+
+def compute_map(data, year, definition, def_args):
+    result = {}
+    for A, B in combinations(data.countries(), 2):
+        link_sign = definition(data, year, A, B, def_args)
+        if link_sign == NO_LINK: continue
+        common_neighbour_count = common_neighbours_count(data, year, A, B, definition, def_args)
+        if common_neighbour_count not in result:
+            result[common_neighbour_count] = (0, 0)
+        (negative_edges, positive_edges) = result[common_neighbour_count]
+        result[common_neighbour_count] = (negative_edges, positive_edges + 1) if link_sign == POSITIVE_LINK\
+        else (negative_edges + 1, positive_edges)
+    return result
+
+
