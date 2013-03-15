43001202684f38a46a4d68d330823276dc331a34
abinesh <td.abinesh@gmail.com>
2013-03-14 22:47:49 -0700

    signed_networks: structural_balance: metrics: vertex: method created

--- project/signed_networks/structural_balance/metrics/main-vertex.py
+++ project/signed_networks/structural_balance/metrics/main-vertex.py
@@ -11,15 +11,19 @@ definition = definition_C3
 def_args1 = args_for_definition_C(10, 5000)
 def_args2 = args_for_definition_C(10, 1000)
 
-print "x=%s;" % str([year for year in data.all_years]).replace(",", " ")
-for A in data.countries():
-    print "degreecount=%s;" % str([degree_count(data, year, A, definition, def_args1) for year in data.all_years]).replace(",", " ")
-    print "degreesum1=%s;" % str([degree_sum(data, year, A, definition, def_args1) for year in data.all_years]).replace(",", " ")
-    print "positivecount1=%s;" % str([positive_edge_count(data, year, A, definition, def_args1) for year in data.all_years]).replace(",", " ")
-    print "negativecount1=%s;" % str([negative_edge_count(data, year, A, definition, def_args1) for year in data.all_years]).replace(",", " ")
-    print "degreesum2=%s;" % str([degree_sum(data, year, A, definition, def_args2) for year in data.all_years]).replace(",", " ")
-    print "plot(x,degreesum1,'b-o',x,degreesum2,'b-*',x,degreecount,'m-o',x,positivecount1,'g-o',x,negativecount1,'r-o');"
-    print "hline = refline([0 0]);"
-    print "set(hline,'Color','b');"
-    print "legend('degree-sum1(T2=5000)','degree-sum2(T2=1000)','degree-count','positivecount1','negativecount1','Location','Best')"
-    print "saveas(gcf,'%s','png');" % file_safe(A)
+def print_degree_sum_over_time():
+    print "x=%s;" % str([year for year in data.all_years]).replace(",", " ")
+    for A in data.countries():
+        print "degreecount=%s;" % str([degree_count(data, year, A, definition, def_args1) for year in data.all_years]).replace(",", " ")
+        print "degreesum1=%s;" % str([degree_sum(data, year, A, definition, def_args1) for year in data.all_years]).replace(",", " ")
+        print "positivecount1=%s;" % str([positive_edge_count(data, year, A, definition, def_args1) for year in data.all_years]).replace(",", " ")
+        print "negativecount1=%s;" % str([negative_edge_count(data, year, A, definition, def_args1) for year in data.all_years]).replace(",", " ")
+        print "degreesum2=%s;" % str([degree_sum(data, year, A, definition, def_args2) for year in data.all_years]).replace(",", " ")
+        print "plot(x,degreesum1,'b-o',x,degreesum2,'b-*',x,degreecount,'m-o',x,positivecount1,'g-o',x,negativecount1,'r-o');"
+        print "hline = refline([0 0]);"
+        print "set(hline,'Color','b');"
+        print "legend('degree-sum1(T2=5000)','degree-sum2(T2=1000)','degree-count','positivecount1','negativecount1','Location','Best')"
+        print "saveas(gcf,'%s','png');" % file_safe(A)
+
+
+print_degree_sum_over_time()
