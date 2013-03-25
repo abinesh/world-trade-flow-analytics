a1fc0d3d4114aa9332d4453594f244576594090f
abinesh <td.abinesh@gmail.com>
2013-03-25 11:57:59 -0700

    signed_networks: structural_balance: metrics: faction: adjacency_matrix test written

--- project/signed_networks/structural_balance/metrics/main-faction.py
+++ project/signed_networks/structural_balance/metrics/main-faction.py
@@ -1,5 +1,5 @@
 from project.config import WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL
-from project.countries import falklands_war_countries
+from project.countries import world_excluded_countries_list
 from project.export_data.exportdata import ExportData
 from project.signed_networks.definitions import definition_C3, args_for_definition_C
 from project.signed_networks.structural_balance.metrics.faction import positives_and_negatives_matrix, adjacency_matrix
@@ -10,6 +10,5 @@ data.load_file('../../' + WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL, should_read_world
 definition = definition_C3
 def_args = args_for_definition_C(10, 5000)
 
-print "xpn=[%s]" % positives_and_negatives_matrix(data, definition, def_args, [1981, 1982, 1983, 1984],
-                                                  falklands_war_countries)
-print "xad=[%s]" % adjacency_matrix(data, definition, def_args, 1982)
+print "xpn=[%s]" % positives_and_negatives_matrix(data, definition, def_args, [1981, 1982, 1983, 1984])
+print "xad=[%s]" % adjacency_matrix(data, definition, def_args, 1982,world_excluded_countries_list())
