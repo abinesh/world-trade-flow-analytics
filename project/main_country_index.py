4068fcd2f281231ce9faf6001de16baa16267d3a
abinesh <td.abinesh@gmail.com>
2013-03-27 02:09:00 -0700

    signed_networks: structural_balance: metrics: faction: detect_community function written

--- project/main_country_index.py
+++ project/main_country_index.py
@@ -2,4 +2,4 @@ from project.countries import index_of_country, falklands_war_countries
 
 
 print index_of_country("USA")
-print "\n".join(["%s,%d" % (C, index_of_country(C) + 1) for C in falklands_war_countries])
+print "\n".join(["%s,%d" % (C, index_of_country(C)) for C in falklands_war_countries])
--- project/signed_networks/structural_balance/metrics/main-faction.py
+++ project/signed_networks/structural_balance/metrics/main-faction.py
@@ -1,9 +1,21 @@
 from numpy import corrcoef
+from project import countries
 from project.config import WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL
 from project.export_data.exportdata import ExportData
 from project.signed_networks.definitions import definition_C3, args_for_definition_C
 from project.signed_networks.structural_balance.metrics.faction import positives_and_negatives_matrix_matlab, adjacency_matrix_matlab, positives_and_negatives_matrix, adjacency_matrix, adjacency_matrix_row
 from project.util import transpose
+
+
+def detect_community(correlation, threshold):
+    result = []
+    for i in range(0, len(correlation)):
+        community = sorted([(countries.index_to_country_map[j], correlation[i][j])
+                            for j in range(0, len(correlation)) if i != j and correlation[i][j] > threshold],
+                           key=lambda t: -t[1])
+        result.append([(a, float("%.2f" % b)) for (a, b) in community])
+    return result
+
 
 data = ExportData()
 data.load_file('../../' + WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL, should_read_world_datapoints=True)
@@ -12,13 +24,25 @@ definition = definition_C3
 def_args = args_for_definition_C(10, 5000)
 
 print "xpn=[%s]" % positives_and_negatives_matrix_matlab(data, definition, def_args, [1981, 1982, 1983, 1984])
-print "xad=[%s]" % adjacency_matrix_matlab(data, definition, def_args, 1982)
-
 print corrcoef(transpose(positives_and_negatives_matrix(data, definition, def_args, [1981, 1982, 1983, 1984])))
+
+'''
+print "xad=[%s]" % adjacency_matrix_matlab(data, definition, def_args, 1982)
 print corrcoef(adjacency_matrix(data, definition, def_args, 1982))
 
 print adjacency_matrix_row(data, definition, def_args, 1970, "China")
 print adjacency_matrix_row(data, definition, def_args, 1990, "China")
 
 print data.countries()
+'''
+
+count = 0
+# for row in detect_community(corrcoef(adjacency_matrix(data, definition, def_args, 1980)), 0):
+for row in detect_community(corrcoef(adjacency_matrix(data, definition, def_args, 1983)), 0):
+# for row in detect_community(corrcoef(positives_and_negatives_matrix(data, definition, def_args, [1978,1979,1980,1981])),0.5):
+# for row in detect_community(corrcoef(positives_and_negatives_matrix(data, definition, def_args, [1982,1983,1984,1985])),0.5):
+    print "%d:%s,%d,%s" % (count, countries.index_to_country_map[count], len(row), row)
+    count += 1
+
+
 
