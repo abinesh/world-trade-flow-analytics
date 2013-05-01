0c99a715cd068274bf920900f3cef6ea155dd86d
abinesh <td.abinesh@gmail.com>
2013-05-01 00:10:44 -0700

    signed_networks: link_prediction: link.py: all curves in single plot

--- project/signed_networks/link_prediction/background_metrics.py
+++ project/signed_networks/link_prediction/background_metrics.py
@@ -4,15 +4,17 @@ from project.signed_networks.definitions import definition_C3, args_for_definiti
 from project.signed_networks.link_prediction.link import percentage_of_new_edges_over_time, percentage_of_edge_sign_changes_over_time
 
 
-def edge_proportion_over_time(data, definition, def_args, function, look_back_duration):
-    x_axis = data.all_years[look_back_duration:]
-    print "x=%s;" % str(x_axis).replace(",", "")
-    print "y=%s;" % str(
-        [function(data, definition, def_args, year, look_back_duration) for year in x_axis]).replace(",", "")
-    print "plot(x,y);"
+def edge_proportion_over_time(data, definition, def_args, function, look_back_durations):
+    for look_back_duration in look_back_durations:
+        x_axis = data.all_years[look_back_duration:]
+        print "x%d=%s;" % (look_back_duration, str(x_axis).replace(",", ""))
+        print "y%d=%s;" % (look_back_duration, str(
+            [function(data, definition, def_args, year, look_back_duration) for year in x_axis]).replace(",", ""))
+    print "plot(x1,y1,x5,y5,x10,y10,x20,y20);"
+    print "legend('1y','5y','10y','20y')"
     print "xlabel('Year');"
     print "ylabel('Percent of new edges');"
-    print "saveas(gcf,'%s-%d-years','png');" % (str(function).split(" ")[1], look_back_duration)
+    print "saveas(gcf,'%s','png');" % (str(function).split(" ")[1])
 
 
 definition = definition_C3
@@ -21,7 +23,6 @@ def_args = args_for_definition_C(10, 5000)
 data = ExportData()
 data.load_file('../' + WORLD_TRADE_FLOW_DATA_FILE_ORIGINAL, should_read_world_datapoints=True)
 
-for function in [percentage_of_edge_sign_changes_over_time]:
-    for look_back_duration in [1, 5, 10, 20]:
-        edge_proportion_over_time(data, definition, def_args, function, look_back_duration)
+for function in [percentage_of_new_edges_over_time, percentage_of_edge_sign_changes_over_time]:
+    edge_proportion_over_time(data, definition, def_args, function, [1, 5, 10, 20])
 
